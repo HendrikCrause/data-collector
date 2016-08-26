751da45255a317b30b7dda8c93a1dc27e5f9634a
Hendrik Crause <wimpie.crause@gmail.com>
2016-08-26 07:37:48 +0200

    Initial source setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./src/main.js",
+  output: {
+    path: __dirname + "/js",
+    filename: "scripts.min.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ["add-module-exports", "react-html-attrs",
+          "transform-class-properties", "transform-decorators-legacy"]
+        }
+      }
+    ]
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
